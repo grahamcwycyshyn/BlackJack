be7074aaedf5caafca572ac9f4b8b4c09400e99f
vincelane1994 <vincelane1994@gmail.com>
2019-08-16 09:11:04 -0400

    User DB set up

--- src/main/java/co/grandcircus/blackjack/entity/User.java
+++ src/main/java/co/grandcircus/blackjack/entity/User.java
@@ -1,12 +1,28 @@
 package co.grandcircus.blackjack.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Long bankroll;
 	private int wins;
 	private int losses;
 	private Long highestPayout;
+	private double winLoss;
+	
+	
+	public void setWinLoss(double winLoss) {
+		this.winLoss = winLoss;
+	}
 	public Long getId() {
 		return id;
 	}
