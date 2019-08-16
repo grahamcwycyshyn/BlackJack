acdebeea58c263a12f20eb0afbfb0c5780d881a0
vincelane1994 <vincelane1994@gmail.com>
2019-08-15 21:48:46 -0400

    Finished Card value method and Hand Value methods(3 points)

--- src/main/java/co/grandcircus/blackjack/entity/Card.java
+++ src/main/java/co/grandcircus/blackjack/entity/Card.java
@@ -1,5 +1,7 @@
 package co.grandcircus.blackjack.entity;
 
+import java.util.List;
+
 public class Card {
 
 	private String value;
@@ -24,5 +26,34 @@ public class Card {
 	public void setSuit(String suit) {
 		this.suit = suit;
 	}
+	public int getHandValue(List<Card> hand) {
+		int score = 0;
+		int aceCount = acesInHand(hand);
+		for(int i = 0; i < hand.size(); i++) {
+			if(hand.get(i).getValue().equalsIgnoreCase("ACE")) {
+				score += 1;
+			} else if(hand.get(i).getValue().equalsIgnoreCase("KING")||hand.get(i).getValue().equalsIgnoreCase("QUEEN")||hand.get(i).getValue().equalsIgnoreCase("JACK")) {
+				score += 10;
+			} else {
+				score += Integer.parseInt(hand.get(i).getValue());
+			}
+		}
+		for(int i = aceCount; i > 0; i--) {
+			
+		}
+		
+		
+		return score;
+	}
+	public int acesInHand(List<Card> hand) {
+		int aceCount = 0;
+		for(int i = 0; i < hand.size(); i++) {
+			if(hand.get(i).getValue().equalsIgnoreCase("ACE")) {
+				aceCount++;
+			}
+		}
+		return aceCount;
+	}
+	
 }
 
