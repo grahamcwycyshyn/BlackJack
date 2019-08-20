c4752514f2260aa1bd780387c458c27c5bff128c
vincelane1994 <vincelane1994@gmail.com>
2019-08-20 09:47:15 -0400

    moved methods

--- src/main/java/co/grandcircus/blackjack/BlackjackController.java
+++ src/main/java/co/grandcircus/blackjack/BlackjackController.java
@@ -19,22 +19,18 @@ import co.grandcircus.blackjack.entity.Deck;
 import co.grandcircus.blackjack.entity.User;
 import co.grandcircus.blackjack.entity.Hand;
 
-
-
-
 //Note: need to change bet method when pulling in multiple users, currently only takes in one user
 @RestController
 public class BlackjackController {
-	
+
 	@Autowired
 	ApiService a;
-	
+
 	@Autowired
 	UserRepository userDao;
-	
+
 	@Autowired
 	HandRepository handDao;
-
 
 	@RequestMapping("/")
 	public ModelAndView index(HttpSession session) {
@@ -45,11 +41,10 @@ public class BlackjackController {
 		System.out.println(session.getAttribute("deck"));
 		return m;
 	}
-	
+
 	@RequestMapping("/game")
-	public ModelAndView game(
-			@RequestParam(value="id", required=false) String id,
-			@SessionAttribute(name="deck", required = false)Deck deck, HttpSession session) {
+	public ModelAndView game(@RequestParam(value = "id", required = false) String id,
+			@SessionAttribute(name = "deck", required = false) Deck deck, HttpSession session) {
 		Long i = (long) 1;
 		User user = userDao.findById(i).get();
 		ModelAndView m = new ModelAndView("game");
@@ -59,13 +54,12 @@ public class BlackjackController {
 		System.out.println(session.getAttribute("deck"));
 		return m;
 	}
-	
 
 	@RequestMapping("/signup")
 	public ModelAndView showSignup() {
 		return new ModelAndView("signup-form");
 	}
-	
+
 	@RequestMapping("/signup-confirmation")
 	public ModelAndView submitSignup(User user, HttpSession session) {
 		userDao.save(user);
@@ -73,14 +67,13 @@ public class BlackjackController {
 		ModelAndView mv = new ModelAndView("thanks");
 		return mv;
 	}
-	
+
 //	@RequestMapping("/login")
 //	public ModelAndView showLogin() {
 //		return new ModelAndView("login-form");
 //	}
 	@RequestMapping("/deal")
-	public ModelAndView deal(HttpSession session, 
-			@SessionAttribute(name="deck", required = false)Deck deck,
+	public ModelAndView deal(HttpSession session, @SessionAttribute(name = "deck", required = false) Deck deck,
 			@RequestParam("betDeal") Integer bet) {
 		session.setAttribute("bet", bet);
 		System.out.println(session.getAttribute("deck"));
@@ -94,25 +87,23 @@ public class BlackjackController {
 		List<Card> userHand = new ArrayList<>();
 		userHand.add(a.getCard(deck.getId()));
 		userHand.add(a.getCard(deck.getId()));
-		Card card = new Card();
+		
 		session.setAttribute("userHand", userHand);
-		session.setAttribute("userHandValue", card.getHandValue(userHand));
+		session.setAttribute("userHandValue", getHandValue(userHand));
 		session.setAttribute("dealerHand", dealerHand);
 		return new ModelAndView("redirect:/game");
 
 	}
-	
+
 	@RequestMapping("/hit")
-	public ModelAndView hit(HttpSession session,
-			@SessionAttribute(name="deck") Deck deck,
-			@SessionAttribute(name="userHand") List<Card> userHand,
-			@SessionAttribute(name="dealerHand") List<Card> dealerHand,
-			@SessionAttribute(name="bet") Integer bet) {
+	public ModelAndView hit(HttpSession session, @SessionAttribute(name = "deck") Deck deck,
+			@SessionAttribute(name = "userHand") List<Card> userHand,
+			@SessionAttribute(name = "dealerHand") List<Card> dealerHand, @SessionAttribute(name = "bet") Integer bet) {
 		userHand.add(a.getCard(deck.getId()));
-		Card card = new Card();
+		
 		session.setAttribute("userHand", userHand);
-		if(card.bust(userHand) == false) {
-		session.setAttribute("userHandValue", card.getHandValue(userHand));
+		if (bust(userHand) == false) {
+			session.setAttribute("userHandValue", getHandValue(userHand));
 		} else {
 			session.setAttribute("userHandValue", "BUST!");
 			Long id = (long) 1;
@@ -123,29 +114,26 @@ public class BlackjackController {
 		}
 		return new ModelAndView("redirect:/game");
 
-		
-
 	}
-	
+
 	@RequestMapping("/stay")
-			public ModelAndView stay(HttpSession session,
-					@SessionAttribute(name="userHand") List<Card> userHand,
-					@SessionAttribute(name= "dealerHand") List<Card> dealerHand,
-					@SessionAttribute(name="deck") Deck deck,
-					@SessionAttribute(name="bet") Integer bet){
-				
-				session.setAttribute("userHand", userHand);
-				Card card = new Card();
-				while(card.dealerHit(dealerHand) == true) {
-					dealerHand.add(a.getCard(deck.getId()));
-				} if(card.bust(dealerHand) == true && card.bust(userHand) == false) {
-					Long id = (long) 1;
-					User user = userDao.findById(id).get();
-					user.setBankroll(user.getBankroll() + bet);
-				}
-				return new ModelAndView("redirct:/game");
-			}
+	public ModelAndView stay(HttpSession session, @SessionAttribute(name = "userHand") List<Card> userHand,
+			@SessionAttribute(name = "dealerHand") List<Card> dealerHand, @SessionAttribute(name = "deck") Deck deck,
+			@SessionAttribute(name = "bet") Integer bet) {
+
+		session.setAttribute("userHand", userHand);
 		
+		while (dealerHit(dealerHand) == true) {
+			dealerHand.add(a.getCard(deck.getId()));
+		}
+		if (bust(dealerHand) == true && bust(userHand) == false) {
+			Long id = (long) 1;
+			User user = userDao.findById(id).get();
+			user.setBankroll(user.getBankroll() + bet);
+		}
+		return new ModelAndView("redirect:/game");
+	}
+
 //	@RequestMapping("/bet")
 //	public ModelAndView bet(@RequestParam(value="bankroll", required=true) Long bet) {
 //		ModelAndView m = new ModelAndView("index");
@@ -156,7 +144,105 @@ public class BlackjackController {
 //		userDao.save(user);
 //		return m;
 //	}
-	
-	
-		
+	public int getHandValue(List<Card> hand) {
+		int score = 0;
+		int aceCount = acesInHand(hand);
+		for (int i = 0; i < hand.size(); i++) {
+			if (hand.get(i).getValue().equalsIgnoreCase("ACE")) {
+				score += 1;
+			} else if (hand.get(i).getValue().equalsIgnoreCase("KING")
+					|| hand.get(i).getValue().equalsIgnoreCase("QUEEN")
+					|| hand.get(i).getValue().equalsIgnoreCase("JACK")) {
+				score += 10;
+			} else {
+				score += Integer.parseInt(hand.get(i).getValue());
+			}
+		}
+		for (int i = aceCount; i > 0; i--) {
+			if (score + (i * 10) < 22) {
+				score += 10;
+			}
+		}
+		return score;
+	}
+
+	public int acesInHand(List<Card> hand) {
+		int aceCount = 0;
+		for (int i = 0; i < hand.size(); i++) {
+			if (hand.get(i).getValue().equalsIgnoreCase("ACE")) {
+				aceCount++;
+			}
+		}
+		return aceCount;
+	}
+
+	public boolean isValueSoft(List<Card> hand) {
+		int score = 0;
+		int aceCount = acesInHand(hand);
+		for (int i = 0; i < hand.size(); i++) {
+			if (hand.get(i).getValue().equalsIgnoreCase("ACE")) {
+				score += 1;
+			} else if (hand.get(i).getValue().equalsIgnoreCase("KING")
+					|| hand.get(i).getValue().equalsIgnoreCase("QUEEN")
+					|| hand.get(i).getValue().equalsIgnoreCase("JACK")) {
+				score += 10;
+			} else {
+				score += Integer.parseInt(hand.get(i).getValue());
+			}
+		}
+		for (int i = aceCount; i > 0; i--) {
+			if (score + (i * 10) < 22) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean dealerHit(List<Card> hand) {
+		boolean isSoft = isValueSoft(hand);
+		int score = getHandValue(hand);
+		if (isSoft && score < 18) {
+			return true;
+		} else if (score < 17) {
+			return true;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean canSplit(List<Card> hand) {
+		if (hand.size() > 2) {
+			return false;
+		} else if (hand.get(0).getValue().equalsIgnoreCase(hand.get(1).getValue())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean canDouble(List<Card> hand) {
+		if (hand.size() > 2) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	public boolean bust(List<Card> hand) {
+		if (getHandValue(hand) > 21) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean canHit(List<Card> hand) {
+		if (!bust(hand)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
