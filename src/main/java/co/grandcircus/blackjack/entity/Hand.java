ad04948c51498e18b8c6d115590bbfbb9332235d
vincelane1994 <vincelane1994@gmail.com>
2019-08-29 11:32:00 -0400

    changed hand value length

--- src/main/java/co/grandcircus/blackjack/entity/Hand.java
+++ src/main/java/co/grandcircus/blackjack/entity/Hand.java
@@ -2,6 +2,7 @@ package co.grandcircus.blackjack.entity;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Hand {
 	private Long handId;
 	private Long userId;
 	private String value;
+	@Column(length = 900)
 	private String hand;
 	transient List<Card> cards;
 	transient int handValue;
